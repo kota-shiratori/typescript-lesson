f7b3b0544fad1055e460bd4fc1cccda08f4400ab
kota_shiratori <swan.kouta@gmail.com>
2024-10-29 06:57:26 +0900

    ✨ オブジェクト

--- boolean-number-string/index.ts
+++ boolean-number-string/index.ts
@@ -1,3 +1,17 @@
 let hasValue: boolean = true;
 let count: number = 10;
-let single: string = 'hello';
+let single: string = "hello";
+
+const person: {
+  name: {
+    first: string;
+    last: string;
+  };
+  age: number;
+} = {
+  name: {
+    first: "Jack",
+    last: "Smith",
+  },
+  age: 21,
+};
