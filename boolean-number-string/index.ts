8c5bd215c3fe2d351c7d157aaa9935c32bed9c5e
kota_shiratori <swan.kouta@gmail.com>
2024-10-29 07:14:01 +0900

    ✨ Enum

--- boolean-number-string/index.ts
+++ boolean-number-string/index.ts
@@ -19,3 +19,18 @@ const person: {
 const fruits: string[] = ["Apple", "Banana", "Grape"];
 
 const book: [string, number, boolean] = ["business", 1500, true];
+
+enum CoffeeSize {
+    SHORT = 'SHORT',
+    TALL = 'TALL',
+    GRANDE = 'GRANDE',
+    VENTI = 'VENTI'
+}
+
+const coffee: {
+    hot: boolean;
+    size: CoffeeSize;
+} = {
+    hot: true,
+    size: CoffeeSize.GRANDE
+}
