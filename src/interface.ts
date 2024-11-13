bd119547f5db42120fa0e1a121e28ad019b337ce
kota_shiratori <swan.kouta@gmail.com>
2024-11-13 23:57:45 +0900

    ✨ インターセクション型

---
+++ src/interface.ts
@@ -0,0 +1,12 @@
+interface Human {
+  name: string;
+  age: number;
+  greeting(message: string): void;
+}
+
+class Developer implements Human {
+    constructor(public name: string,public age: number){}
+    greeting(message: string): void {
+        console.log('hello');
+    }
+}
