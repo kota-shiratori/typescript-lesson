39bc4511e3c26dac688dfea345d32c7ebd536487
kota_shiratori <swan.kouta@gmail.com>
2024-11-14 22:57:54 +0900

    ✨ 型アサーション

--- src/advanced.ts
+++ src/advanced.ts
@@ -15,3 +15,48 @@ const quill: EngineerBlogger = {
   role: "front-end",
   follower: 1000,
 };
+
+function toUpperCase(x: string | number) {
+  if (typeof x === "string") {
+    return x.toUpperCase();
+  }
+  return "";
+}
+
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name);
+  if ("role" in nomadWorker) {
+    console.log(nomadWorker.role);
+  }
+  if ("follower" in nomadWorker) {
+    console.log(nomadWorker.follower);
+  }
+}
+
+class Dog {
+  speak() {
+    console.log("bow-wow");
+  }
+}
+
+class Bird {
+  speak() {
+    console.log("tweet-tweet");
+  }
+  fly() {
+    console.log("flutter");
+  }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+  pet.speak();
+  if (pet instanceof Bird) {
+    pet.fly();
+  }
+}
+
+// const input = <HTMLInputElement>document.getElementById('input');
+const input = document.getElementById('input') as HTMLInputElement;
+input.value = 'input value';
